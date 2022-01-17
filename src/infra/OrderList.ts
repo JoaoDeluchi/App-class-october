33d094849e8448a83708013905bc31075ad113a0
Joao Victor Guimaraes Deluchi <joao.deluchi@hbsis.com.br>
2022-01-17 19:42:12 -0300

    feat: create beer and tested products

--- src/infra/OrderList.ts
+++ src/infra/OrderList.ts
@@ -1,12 +1,26 @@
-import { IOrder } from "../interfaces/order";
+import { IOrder } from "../interfaces/order"
+import { productDBInstance } from './instance'
 
-export default class Order {
+export class OrderDB {
     orderList: IOrder[] = []
 
     addOrder(newOrder: IOrder){
         this.orderList.push(newOrder)
     }
-    /// deleteOrder , addProductOnList, removeProductOfList
+    /// removeProductOfList
+    addProductOnOrder(productId: string, orderId: string){
+        const [newProduct] = productDBInstance.getProductById(productId)
+        for(let order of this.orderList){
+            if(order.id===orderId){
+                order.products.push(newProduct)
+            }
+        }
+    }
+
+    deleteOrder(orderId: string){
+        this.orderList = this.orderList.filter((order) => order.id !== orderId)
+    }
+    
     updateOrder(orderId: string, updatedOrder: IOrder){
         this.orderList.filter(order => {
             if(order.id === orderId){
--- src/view/products/product.ts
+++ src/view/products/product.ts
@@ -1,9 +1,20 @@
+import { productDBInstance } from '../../infra/instance'
+/// Router é a classe que cria objeto com os http methods
+/// Request Response types do express que podem ser importados após instalação do @types/express
 import { Router, Request, Response } from 'express'
-
+import { Beer } from '../../products/Beer'
 const route = Router()
 
-route.get('/products', (req: Request, res: Response) => {
-    res.json({parabens: 'funcou essa mizéria' })
+route.get('/products', (_: Request, res: Response) => {
+    res.json(productDBInstance.productList)
 })
+
+route.post('/product/beer',(req: Request, res: Response) => {
+    const { price, description } = req.body
+    const newBeer = new Beer(price, description)
+    productDBInstance.createNewProduct(newBeer)
+    res.json(productDBInstance.productList)
+})
+
 
 export { route }
